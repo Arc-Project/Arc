26cc512c7ae06c54dc0ab4e46f8175c89ef510dd
Hank Lo <Hlo1453@gmail.com>
2016-05-06 15:56:34 -0700

    Searching employees, listing all employees

--- ArcApp/mainwindow.h
+++ ArcApp/mainwindow.h
@@ -131,7 +131,9 @@ private slots:
 
     void on_btn_floatCount_clicked();
 
-    void on_btn_listAllUsers_2_clicked();
+    void on_btn_listAllUsers_clicked();
+
+    void on_btn_searchUsers_clicked();
 
 private:
     Ui::MainWindow *ui;
