ae937a4074bf8e027f6ddea163957f3286483e32
Dennis <dennis.chau@outlook.com>
2016-05-14 12:16:37 -0700

    status bar shows user and shift

--- ArcApp/mainwindow.cpp
+++ ArcApp/mainwindow.cpp
@@ -47,6 +47,11 @@ MainWindow::MainWindow(QWidget *parent) :
 
     MainWindow::setupReportsScreen();
 
+    //display logged in user and current shift in status bar
+    QLabel *lbl_curUser = new QLabel("Logged in as:   ");
+    QLabel *lbl_curShift = new QLabel("Shift Number: ");
+    statusBar()->addPermanentWidget(lbl_curUser);
+    statusBar()->addPermanentWidget(lbl_curShift);
 }
 
 MainWindow::~MainWindow()
