4c2292b9e36ba1aee09e4042cde58d8f43433426
imoongom <imoongom@gmail.com>
2016-05-15 20:03:39 -0700

    add total count of transaction and booking

--- ArcApp/mainwindow.cpp
+++ ArcApp/mainwindow.cpp
@@ -25,8 +25,8 @@ QVector<QString> pcpTypes;
 bool loaded = false;
 QString idDisplayed;
 
-int transacNum;
-int bookingNum;
+int transacNum, transacTotal;
+int bookingNum, bookingTotal;
 
 QProgressDialog* dialog;
 
@@ -1183,13 +1183,12 @@ void MainWindow::on_pushButton_RegisterClient_clicked()
 void MainWindow::on_pushButton_editClientInfo_clicked()
 {
     addHistory(CLIENTLOOKUP);
+    getCurrentClientId();
+
     ui->stackedWidget->setCurrentIndex(CLIENTREGISTER);
     ui->label_cl_infoedit_title->setText("Edit Client Information");
     ui->button_register_client->setText("Edit");
-    int nRow = ui->tableWidget_search_client->currentRow();
-    if (nRow <0)
-        return;
-    curClientID = ui->tableWidget_search_client->item(nRow, 0)->text();
+    getCurrentClientId();
 }
 void MainWindow::on_button_cancel_client_register_clicked()
 {
@@ -1202,6 +1201,14 @@ void MainWindow::on_reportsButton_clicked()
     ui->stackedWidget->setCurrentIndex(REPORTS);
     addHistory(MAINMENU);
     qDebug() << "pushed page " << MAINMENU;
+}
+
+void MainWindow::getCurrentClientId(){
+    int nRow = ui->tableWidget_search_client->currentRow();
+    if (nRow <0)
+        return;
+    curClientID = ui->tableWidget_search_client->item(nRow, 0)->text();
+
 }
 
 /*===================================================================
@@ -1403,7 +1410,7 @@ void MainWindow::on_button_register_client_clicked()
         {
             if (dbManager->updateClientWithPic(&registerFieldList, curClientID, &profilePic))
             {
-                statusBar()->showMessage("Client Registered Sucessfully.");
+                statusBar()->showMessage("Client information updated Sucessfully.");
                 qDebug() << "Client info edit successfully";
                 clear_client_register_form();
                 ui->stackedWidget->setCurrentIndex(1);
@@ -1538,6 +1545,8 @@ void MainWindow::selected_client_info(int nRow, int nCol)
     curClientID = ui->tableWidget_search_client->item(nRow, 0)->text();
     transacNum = 5;
     bookingNum = 5;
+    transacTotal = dbManager->countInformationPerClient("Transac", curClientID);
+    bookingTotal = dbManager->countInformationPerClient("booking", curClientID);
     ui->pushButton_cl_trans_more->setEnabled(true);
 
     table_available = false;
@@ -1561,11 +1570,6 @@ void MainWindow::displayClientInfoThread(QString val){
     qDebug()<<"DISPLAY THREAD: " <<val;
 
     QSqlQuery clientInfo = dbManager->searchClientInfo(val);
-//    QString searchQuery = "SELECT FirstName, MiddleName, LastName, Dob, Balance, SinNo, GaNo, DateRulesSigned, status FROM Client WHERE ClientId =" + val;
-
-    // QString searchQuery = "SELECT FirstName, MiddleName, LastName, Dob, Balance FROM Client WHERE ClientId =" + val;
-//    QSqlQuery clientInfoR = dbManager->execQuery(searchQuery);
-
 
    clientInfo.next();
 
@@ -1694,7 +1698,6 @@ void MainWindow::on_tabWidget_cl_info_currentChanged(int index)
             if(transacFuture.isRunning()|| !transacFuture.isFinished()){
                 qDebug()<<"TransactionHistory Is RUNNING";
                 return;
-                //displayFuture.cancel();
             }
             ui->pushButton_cl_trans_more->setEnabled(true);
             transacFuture = QtConcurrent::run(this, &searchTransaction, curClientID);
@@ -1706,12 +1709,10 @@ void MainWindow::on_tabWidget_cl_info_currentChanged(int index)
              if(bookHistoryFuture.isRunning()|| !bookHistoryFuture.isFinished()){
                  qDebug()<<"BookingHistory Is RUNNING";
                  return;
-                 //displayFuture.cancel();
              }
              ui->pushButton_cl_book_more->setEnabled(true);
              bookHistoryFuture = QtConcurrent::run(this, &searchBookHistory, curClientID);
              bookHistoryFuture.waitForFinished();
-             qDebug()<<"client Transaction list";
 
     }
 }
@@ -1730,6 +1731,8 @@ void MainWindow::searchTransaction(QString clientId){
     QSqlQuery transQuery = dbManager->searchClientTransList(transacNum, clientId);
     displayTransaction(transQuery);
 
+    QString totalNum= (transacTotal == 0)? "-" : QString::number(transacTotal);
+    ui->label_cl_trans_total_num->setText(totalNum + " Transaction");
 }
 
 
@@ -1751,16 +1754,15 @@ void MainWindow::displayTransaction(QSqlQuery results){
         }
         row++;
     }
-    if(row !=transacNum || row%5 != 0){
+    if(row > transacTotal || row == transacTotal){
         ui->pushButton_cl_trans_more->setEnabled(false);
     }
-    ui->tableWidget_transaction->setMinimumHeight(37*row-5);
+    ui->tableWidget_transaction->setMinimumHeight(35*row-5);
 
 }
 
 void MainWindow::on_pushButton_cl_trans_more_clicked()
 {
-
     transacNum +=5;
     searchTransaction(curClientID);
 }
@@ -1771,7 +1773,8 @@ void MainWindow::searchBookHistory(QString clientId){
     QSqlQuery bookingQuery = dbManager->searchTransBookList(bookingNum, clientId);
     displayBookHistory(bookingQuery);
     dbManager->printAll(bookingQuery);
-
+    QString totalNum = (bookingTotal == 0)? "-" : QString::number(bookingTotal);
+    ui->label_cl_booking_total_num->setText(totalNum + " Booking");
 }
 
 void MainWindow::displayBookHistory(QSqlQuery results){
@@ -1791,10 +1794,10 @@ void MainWindow::displayBookHistory(QSqlQuery results){
         }
         row++;
     }
-    if(row !=bookingNum || row%5 != 0){
+    if(row > bookingTotal || row == bookingTotal){
         ui->pushButton_cl_book_more->setEnabled(false);
     }
-    ui->tableWidget_booking->setMinimumHeight(37*row-5);
+    ui->tableWidget_booking->setMinimumHeight(35*row-5);
 
 }
 
