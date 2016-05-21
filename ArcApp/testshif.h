2243126c2e3a6e7e69f509644b720c63d0d715b9
imoongom <imoongom@gmail.com>
2016-05-21 04:52:02 -0700

    fix recent transaction table height

---
+++ ArcApp/testshif.h
@@ -0,0 +1,29 @@
+#ifndef TESTSHIF_H
+#define TESTSHIF_H
+
+#include <QAbstractItemModel>
+
+class testShif : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+    explicit testShif(QObject *parent = 0);
+
+    // Header:
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    // Basic functionality:
+    QModelIndex index(int row, int column,
+                      const QModelIndex &parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex &index) const override;
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+};
+
+#endif // TESTSHIF_H
